f4b12da259532b426709f8f90fcaefab7f021e8a
OrkenJR <orkenfreeman@gmail.com>
2022-10-16 11:21:13 +0600

    feat: increment cfas-lib

--- src/main/java/kz/iitu/usermanagement/UserManagementApplication.java
+++ src/main/java/kz/iitu/usermanagement/UserManagementApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class})
 @EnableEurekaClient
 public class UserManagementApplication {
 
