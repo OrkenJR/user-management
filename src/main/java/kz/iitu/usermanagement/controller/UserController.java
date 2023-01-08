2044502e959a7d682a83e0435d4a54b9489fea8b
Orken <orkenfreeman@gmail.com>
2023-01-08 11:11:31 +0600

    feat: added swagger

--- src/main/java/kz/iitu/usermanagement/controller/UserController.java
+++ src/main/java/kz/iitu/usermanagement/controller/UserController.java
@@ -1,5 +1,7 @@
 package kz.iitu.usermanagement.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import kz.iitu.cfaslib.controller.AbstractWrapper;
 import kz.iitu.usermanagement.model.User;
 import kz.iitu.usermanagement.service.UserService;
@@ -16,27 +18,32 @@ import java.util.List;
  *
  * @author Orken
  **/
+@Tag(name = "User", description = "The User API")
 @RestController
 @RequiredArgsConstructor
 public class UserController extends AbstractWrapper {
 
     private final UserService userService;
 
+    @Operation(summary = "Getting user by username")
     @GetMapping("/username/{username}")
     public ResponseEntity<User> byUsername(@PathVariable String username) {
         return responseWrap(userService.byUsername(username));
     }
 
+    @Operation(summary = "Getting list of user, soon will be deprecated")
     @GetMapping("/users")
     public ResponseEntity<List<User>> findAll() {
         return responseWrap(userService::findAll);
     }
 
+    @Operation(summary = "Getting list of subordinates")
     @GetMapping("/users/subordinates/{parent}")
     public ResponseEntity<List<User>> childrenByParent(@PathVariable String parent) {
         return responseWrap(() -> userService.getChildrenByParent(parent));
     }
 
+    @Operation(summary = "Getting list of bosses")
     @GetMapping("/users/chef/{children}")
     public ResponseEntity<User> parentByChildren(@PathVariable String children) {
         return responseWrap(userService.getParentByChildren(children));
